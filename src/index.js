8c497b281c59f53a970efb21d91736a94bdb8b9a
Plastikaweb <info@plastikaweb.com>
2017-01-13 08:11:24 +0100

    index.js init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var uniqueRandomArray = require('unique-random-array'),
+    starWarsNames = require('./starwars-names.json');
+
+module.exports = {
+    all: starWarsNames,
+    random: uniqueRandomArray(starWarsNames)
+};
